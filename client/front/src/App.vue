308581794ecea7a154f5135a6f6dd788dfea2e75
vladislav bobyrev <perets1995@gmail.com>
2022-07-06 23:18:08 +0300

    :construction_worker: создал базовую разметку для приложения

--- client/front/src/App.vue
+++ client/front/src/App.vue
@@ -16,24 +16,60 @@
       <div class="main-body">
         <ul class="tasks">
           <li class="task">
-            <label>
-              <input type="checkbox">
-            </label>
-           <div class="tack-info" >
-             <span class="task__text">Сайт с статьями ts vue + админка</span>
-             <span class="task__date">5</span>
+            <div class="flex">
+                  <label>
+                    <input type="checkbox">
+                  </label>
+                <div class="tack-info" >
+                  <span class="task__text">колбаса</span>
+                  <span class="task__date">5.12.22 г.</span>
+                </div>
+            </div>
+
+            <div class="flex">
+                <div class="tack-info">
+                  <label for="price">стоимость</label>
+                  <input class="price" name='price' type="text" placeholder="123">
+                </div>
+
+                <div class="tack-info">
+                    <label for="count">count</label>
+                  <div>
+                    <button>+</button>
+                    <input type="text" name="count" placeholder="1">
+                    <button>-</button>
+                </div>
+            </div>
            </div>
            <div class="task__edit icon">
             <svg width="24" height="24"><g fill="none" fill-rule="evenodd"><path fill="currentColor" d="M9.5 19h10a.5.5 0 110 1h-10a.5.5 0 110-1z"></path><path stroke="currentColor" d="M4.42 16.03a1.5 1.5 0 00-.43.9l-.22 2.02a.5.5 0 00.55.55l2.02-.21a1.5 1.5 0 00.9-.44L18.7 7.4a1.5 1.5 0 000-2.12l-.7-.7a1.5 1.5 0 00-2.13 0L4.42 16.02z"></path></g></svg>
            </div>
           </li>
-          <li class="task">
-            <label>
-              <input type="checkbox">
-            </label>
-           <div class="tack-info" >
-             <span class="task__text">Сайт с статьями ts vue + админка</span>
-             <span class="task__date">5</span>
+                     <li class="task">
+            <div class="flex">
+                  <label>
+                    <input type="checkbox">
+                  </label>
+                <div class="tack-info" >
+                  <span class="task__text">колбаса</span>
+                  <span class="task__date">5.12.22 г.</span>
+                </div>
+            </div>
+
+            <div class="flex">
+                <div class="tack-info">
+                  <label for="price">стоимость</label>
+                  <input class="price" name='price' type="text" placeholder="123">
+                </div>
+
+                <div class="tack-info">
+                    <label for="count">count</label>
+                  <div>
+                    <button>+</button>
+                    <input type="text" name="count" placeholder="1">
+                    <button>-</button>
+                </div>
+            </div>
            </div>
            <div class="task__edit icon">
             <svg width="24" height="24"><g fill="none" fill-rule="evenodd"><path fill="currentColor" d="M9.5 19h10a.5.5 0 110 1h-10a.5.5 0 110-1z"></path><path stroke="currentColor" d="M4.42 16.03a1.5 1.5 0 00-.43.9l-.22 2.02a.5.5 0 00.55.55l2.02-.21a1.5 1.5 0 00.9-.44L18.7 7.4a1.5 1.5 0 000-2.12l-.7-.7a1.5 1.5 0 00-2.13 0L4.42 16.02z"></path></g></svg>
@@ -52,6 +88,5 @@
 <script lang="ts" setup>
 import TheHeader from './components/TheHeader/TheHeader.vue'
 import TheNav from './components/TheNav/TheNav.vue'
-
 
 </script>
--- client/front/src/components/TheHeader/TheHeader.vue
+++ client/front/src/components/TheHeader/TheHeader.vue
@@ -6,7 +6,7 @@
           <path fill="currentColor" fill-rule="evenodd" d="M4.5 5h15a.5.5 0 1 1 0 1h-15a.5.5 0 0 1 0-1zm0 6h15a.5.5 0 1 1 0 1h-15a.5.5 0 1 1 0-1zm0 6h15a.5.5 0 1 1 0 1h-15a.5.5 0 1 1 0-1z"></path>
         </svg>
       </button>
-      <input class="search" type="search" name="search"/>
+      <input class="search" type="text" name="search" placeholder="Ананас"/>
       <label for="search"></label>
     </div>
   </header>
